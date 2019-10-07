560c991ffd897a691025fe31335857617bb81cd8
Davi-Silva <davi.cunha.silva.ca@gmail.com>
2019-10-07 13:21:51 -0300

    .

--- start_dev_course_mern.sh
+++ start_dev_course_mern.sh
@@ -7,15 +7,10 @@ code .
 yarn init -y
 yarn add concurrently
 awk 'NR>1&&/dependencies/{print "\t\"scripts\": {\n\t\t\"server\": \"cd backend && yarn run dev\",\n\t\t\"client\": \"cd frontend && yarn run start\",\n\t\t\"dev\": \"concurrently \\\"yarn run server\\\" \\\"yarn run client\\\"\"\n\t},"}1' package.json >> package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
-sed -i "1d" package.json
+for i in 1 2 3 4 5 6 7 8 9
+do
+  sed -i "1d" package.json
+done
 echo "Cloning Repositories..."
 git clone "https://github.com/Davi-Silva/course-backend.git"
 git clone "https://github.com/Davi-Silva/course-frontend.git"
