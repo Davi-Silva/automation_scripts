4dacbcb05e24ef848f3e6f9343ce11277b52b88f
Davi-Silva <davi.cunha.silva.ca@gmail.com>
2019-10-25 15:52:22 -0300

    .

--- update_vscode_extensions.sh
+++ update_vscode_extensions.sh
@@ -4,7 +4,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 echo $parent_path
 code --list-extensions | xargs -L 1 echo code --install-extension > vscode_extensions.txt
-git add .
+git add vscode_extensions.txt
 git commit -m "VS Code extensions updated."
 git push
 echo "VS Code extensions file updated."
