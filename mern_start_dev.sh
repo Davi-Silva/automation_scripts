284d1dcb7fa4323f10513c46c06b431337f643d6
Davi Silva <davi.cunha.silva.ca@gmail.com>
2019-12-21 12:39:43 -0300

    added git pull for both frontend and backend on start dev script

--- mern_start_dev.sh
+++ mern_start_dev.sh
@@ -3,6 +3,10 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 cd ..
 [[ -d cryptic-activist ]] || ./automation_scripts/mern_install_dependencies.sh
-cd cryptic-activist
+cd cryptic-activist/frontend
+git pull
+cd ../backend
+git pull
+cd ..
 code .
 yarn run dev
